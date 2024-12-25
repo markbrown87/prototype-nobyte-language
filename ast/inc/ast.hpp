6321e38ad6a74c7ee8aa122961ce5508cd87163b
Mark Brown <mark.brown8790@gmail.com>
2024-12-25 19:45:48 +0000

    chore(skeleton): init code for AST class

---
+++ ast/inc/ast.hpp
@@ -0,0 +1,16 @@
+#ifndef AST_HPP
+#define AST_HPP
+
+// Namespace for Argument Parser
+namespace AST_NS {
+/*
+Abstract Syntax Tree implementation
+*/
+class AST {
+public:
+  AST() = default;
+
+};
+} // namespace AST_NS
+
+#endif
---
+++ ast/tests/unit_tests.cpp
@@ -0,0 +1,9 @@
+#include "ast.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace ARG_NS;
+
+TEST(AST_Class, sample) {
+  EXPECT_TRUE(false);
+}
