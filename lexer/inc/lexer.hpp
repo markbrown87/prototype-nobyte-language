277744b8b9e7be8b082ddbfa9e02a5d9488b3084
Mark Brown <mark.brown8790@gmail.com>
2024-12-26 03:35:34 +0000

    refactor(lexer): adjusted lexing part to process character by character

--- lexer/inc/lexer.hpp
+++ lexer/inc/lexer.hpp
@@ -3,8 +3,8 @@
 
 #include "token_struct.hpp"
 
-#include <sstream>
-#include <vector>
+#include <fstream>
+#include <memory>
 
 // Namespace for Lexer
 namespace LXR_NS {
@@ -13,13 +13,28 @@ Lexer class that parses and creates an array of tokens
 */
 class Lexer {
 public:
-  Lexer() = default;
+  Lexer() = delete;
+  ~Lexer() {
+    p_file.reset();
+  }
+  Lexer(std::ifstream filehandle);
+  Lexer(const Lexer&) = delete;
+  Lexer& operator=(const Lexer&) = delete;
+  Lexer(Lexer&& other) : p_file(std::move(other.p_file)) {}
+  Lexer& operator=(Lexer&& other) {
+    p_file = std::move(other.p_file);
+    return *this;
+  }
 
-  /*
-  Parses the line and returns an array of tokens
-  */
-  auto lex(std::stringstream &stream) const noexcept
-      -> const std::vector<Token>;
+  // Gets the next token in the file
+  auto getNextToken() noexcept -> const Token&;
+
+  // Get the current token
+  auto getCurrentToken() noexcept -> const Token&;
+
+  private:
+  Token m_currentToken;
+  std::unique_ptr<std::ifstream> p_file;
 };
 } // namespace LXR_NS
 
